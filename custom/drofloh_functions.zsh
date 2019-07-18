8a278d5186be1ba4f9f3fc5d22e17a9f9868495c
Andrew Holford <andrewholford@gmail.com>
2019-07-18 06:08:12 +0100

    Move all icons to one place and use hexcodes

--- custom/drofloh_functions.zsh
+++ custom/drofloh_functions.zsh
@@ -1,27 +1,68 @@
 ###############################################################
-# drofloh oh-my-zsh theme
+# drofloh oh-my-zsh theme functions
 # https://github.com/drofloh/oh-my-zsh-custom
 ################################################################
 
+typeset -AHg ICONS
+
+ICONS=(
+  # Prompt separators
+  flame_right        $'\ue0c0' # 
+  flame_left         $'\ue0c2' # 
+  terminal           $'\uf120' # 
+
+  # OS logos
+  apple_logo         $'\uf302' # 
+
+  # directory icon
+  folder             $'\ufc6e' # ﱮ
+
+  # battery usage
+  battery_full       $'\uf578' # 
+  battery_80         $'\uf580' # 
+  battery_60         $'\uf57e' # 
+  battery_40         $'\uf57c' # 
+  battery_20         $'\uf57a' # 
+  battery_empty      $'\uf58d' # 
+  battery_charging   $'\uf0e7' # 
+  battery_plugged_in $'\uf583' # 
+
+  # time / clock icon
+  time               $'\uf49b' # 
+
+  # git status icons
+  git_branch         $'\ue0a0' # 
+  git_added          $'\uf457' # 
+  git_modified       $'\uf459' # 
+  git_deleted        $'\uf458' # 
+  git_renamed        $'\uf45a' # 
+  git_unmerged       $'\ue727' # 
+  git_untracked      $'\uf128' # 
+  git_ahead          $'\uf061' # 
+  git_behind         $'\uf060' # 
+  git_remote_exists  $'\ufadf' # 﫟
+  git_remote_missing $'\uf658' # 
+)
+
 function prompt_separator() {
-  echo ''
+  echo $ICONS[flame_right]
 }
 
 function rprompt_separator() {
-  echo ''
+  echo $ICONS[flame_left]
 }
 
 function prompt_start() {
   local bg_col=%{$BG[236]%}
   
-  local ret_status="%(?:%{$fg[green]%}  :%{$fg[red]%}  )${reset_color}"
+  local ret_status="%(?:%{$fg[green]%} $ICONS[apple_logo] :%{$fg[red]%} $ICONS[apple_logo] )${reset_color}"
   echo "${bg_col}${ret_status}"
 }
 
 function prompt_dir() {
   local bg_col=%{$BG[240]%}
   local fg_col=%{$FG[236]%}
-  local dir_icon="%{$fg[blue]%}   "
+  local dir_icon="%{$fg[blue]%}  $ICONS[folder] "
   local directory="%{$fg[white]%}%~"
 
   echo "${bg_col}${fg_col}$(prompt_separator)${dir_icon}${directory}"
@@ -46,37 +87,37 @@ function prompt_battery() {
 
   battery_perc=$(battery_pct)
   if [ $battery_perc -gt 90 ]; then
-    icon=''
+    icon=$ICONS[battery_full]
     batt_col=$fg[green]
   elif [ $battery_perc -gt 70 ]; then
-    icon=''
+    icon=$ICONS[battery_80]
     batt_col=$fg[green]
   elif [ $battery_perc -gt 50 ]; then
-    icon=''
+    icon=$ICONS[battery_60]
     batt_col=$fg[green]
   elif [ $battery_perc -gt 30 ]; then
-    icon=''
+    icon=$ICONS[battery_40]
     batt_col=$fg[yellow]
   elif [ $battery_perc -gt 10 ]; then
-    icon=''
+    icon=$ICONS[battery_20]
     batt_col=$fg[yellow]
   else
-    icon=''
+    icon=$ICONS[battery_empty]
     batt_col=$fg[red]
   fi
 
   if $(battery_is_charging) ; then
-    power_icon="%{$fg[yellow]%}"
+    power_icon="%{$fg[yellow]%}${ICONS[battery_charging]}"
   elif $(plugged_in) ; then
     power_icon=''
-    icon=''
+    icon=$ICONS[battery_plugged_in]
     batt_col=$fg[green]
   fi
   echo "%{$FG[240]%}$(rprompt_separator) %{$BG[240]%} %{$batt_col%}${icon}${power_icon} %{$fg[white]%}${battery_perc}%%%{$reset_color%}"
 }
 
 function prompt_time() {
-  local time_icon="%{$fg[magenta]%}"
+  local time_icon="%{$fg[magenta]%}${ICONS[time]}"
   local the_time="%{$fg[white]%}[%T]"
 
   echo "%{$FG[244]%}$(rprompt_separator) %{$BG[244]%} ${time_icon} ${the_time}"
--- custom/themes/drofloh.zsh-theme
+++ custom/themes/drofloh.zsh-theme
@@ -13,23 +13,22 @@
 # Should also enable the battery plugin in the main oh-my-zsh 
 # ~/.zshrc
 #
-#                                          
 ################################################################
 
 PROMPT='$(prompt_start)$(prompt_dir)$(prompt_git)
-%{$fg[white]%} $ '
+%{$fg[white]%}${ICONS[terminal]} $ '
 
 RPROMPT='$(prompt_time)$(prompt_battery)'
 
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
-ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[green]%}"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_REMOTE_EXISTS="%{$fg[green]%}﫟 "
-ZSH_THEME_GIT_PROMPT_REMOTE_MISSING="%{$fg[yellow]%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}${ICONS[git_branch]}"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}${ICONS[git_branch]}"
+ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[magenta]%}${ICONS[git_added]}"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[magenta]%}${ICONS[git_modified]}"
+ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[magenta]%}${ICONS[git_deleted]}"
+ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[magenta]%}${ICONS[git_renamed]}"
+ZSH_THEME_GIT_PROMPT_UNMERGED=" %{$fg[magenta]%}${ICONS[git_unmerged]}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED=" %{$fg[magenta]%}${ICONS[git_untracked]}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[green]%}${ICONS[git_ahead]}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[yellow]%}${ICONS[git_behind]}"
+ZSH_THEME_GIT_PROMPT_REMOTE_EXISTS="%{$fg[green]%}${ICONS[git_remote_exists]} "
+ZSH_THEME_GIT_PROMPT_REMOTE_MISSING="%{$fg[yellow]%}${ICONS[git_remote_missing]} "
